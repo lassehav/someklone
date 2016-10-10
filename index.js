1b4d0373ec854c154cf4d314e36cd0407059843c
Lasse Haverinen <lasse.haverinen@gmail.com>
2016-10-10 17:41:42 +0300

    cleanup

--- index.js
+++ index.js
@@ -26,81 +26,6 @@ app.use('/users', userRoutes.router);
 app.use('/posts', postRoutes.router);
 
 
-/*
-
-app.get('/posts/user/:userId', function(req, res, next){    
-    models.User.findById(req.params.userId)
-    .getUserPosts()
-    .then(function(s) {
-        res.json(s);
-    }); 
-})
-
-app.post('/users', function(req,res,next){
-    models.User.create({
-        username: "Test",
-        profileImageSmall: "http://core0.staticworld.net/images/article/2015/11/111915blog-donald-trump-100629006-primary.idge.jpg"    
-    }).then(function(i) {
-        res.json({
-            id: i.dataValues.id
-        });        
-    });
-});
-
-app.get('/users', function(req,res,next){
-    models.User.findAll().then(function(u){
-        res.json(u);
-    })
-});
-
-app.post('/posts', function(req,res,next){
-    models.User.findById(1).then(function(u){
-        u.createPost({
-            image: "https://lh4.ggpht.com/wKrDLLmmxjfRG2-E-k5L5BUuHWpCOe4lWRF7oVs1Gzdn5e5yvr8fj-ORTlBF43U47yI=w300",        
-            imageThumbnail: "https://lh4.ggpht.com/wKrDLLmmxjfRG2-E-k5L5BUuHWpCOe4lWRF7oVs1Gzdn5e5yvr8fj-ORTlBF43U47yI=w300",        
-            caption: "Jelloo"})        
-        .then(function(p) { 
-            p.dataValues.User = u;           
-            res.json(p);                                
-        });       
-    });    
-});
-
-app.post('/posts/:postId/likes/:userId', function(req,res,next){
-    Promise.all([
-        models.User.findById(req.params.userId),
-        models.Post.findById(req.params.postId)
-    ]).then(function(results){
-        var u = results[0];
-        var p = results[1];
-
-        p.addLike(u).then(function(likeRes){
-            res.json(likeRes);
-        })
-        .catch(function(err){
-            console.log(err);
-            res.sendStatus(500);
-        });
-        
-    })
-});
-
-app.get('/posts/:postId/likes/count',function(req,res,next){
-
-    models.Post.findById(req.params.postId).then(function(post){
-        post.countLikes().then(function(l){
-            res.json(l);
-        });
-        
-    });    
-});
-
-
-*/
-
-/* Seeding */
-
-
 //models.sequelize.sync({force: true}).then(function() {
 models.sequelize.sync().then(function() {
     
@@ -109,8 +34,3 @@ models.sequelize.sync().then(function() {
     });
     
 });
-
-
-
-
-
